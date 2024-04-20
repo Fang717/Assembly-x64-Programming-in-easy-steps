2b950ce08da45d3c24a22e6842f52cec0648d8fe
Fang717 <yura717@gmail.com>
2024-04-20 00:18:07 -0600

    Add files via upload

---
+++ CREATE/CREATE.asm
@@ -0,0 +1,36 @@
+;--------------------------------------------------------------------
+; CREATE: A program to demonstrate file creation.
+;--------------------------------------------------------------------
+
+
+
+section .data
+    filePath db "/home/yuriy/Quote.txt", 0 ; Path of file to write into.
+    fileHandle dq 0 ; Variable to store file handle.
+
+section .text
+    global _start
+
+_start:
+
+; Create file
+    mov rax, 2          ; sys_open syscall
+    mov rdi, filePath   ; path
+    mov rsi, 0x241      ; O_CREAT | O_WRONLY
+    mov rdx, 0644o      ; file mode
+    syscall
+    mov [fileHandle], rax ; store file handle
+
+
+; Close file
+    mov rax, 3          ; sys_close syscall
+    mov rdi, [fileHandle] ; file handle
+    syscall
+
+
+
+
+; Exit the program
+    mov rax, 60             ; sys_exit system call
+    xor rdi, rdi            ; Exit status 0
+    syscall
